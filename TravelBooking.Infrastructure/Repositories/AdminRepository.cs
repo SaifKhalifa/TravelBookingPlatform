7d6ddc72e12193e8f9c6c0ca837e4bd347f1fa6a
Saif Khalifa <63987779+SaifKhalifa@users.noreply.github.com>
2025-08-06 21:38:08 +0300

    Add AdminService and repository interfaces for hotel management
    This commit introduces the `AdminService` class implementing the `IAdminService` interface, which includes methods for creating hotels, rooms, room types, and discounts.
    
    The service interacts with the data layer through the `IAdminRepository`, which has been defined to check for existing cities and hotels, as well as to add new entities to the database.
    
    The `Program.cs` file is updated to register the `IAdminRepository` and `AdminRepository` with the dependency injection container.
    
    **NOTE: `AdminController` will be refactored later as it depends on most of the other services and their CRUD operations.

---
+++ TravelBooking.Infrastructure/Repositories/AdminRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelBooking.Application.Interfaces;
+using TravelBooking.Domain.Entities;
+using TravelBooking.Infrastructure.Persistence;
+
+namespace TravelBooking.Infrastructure.Repositories;
+
+public class AdminRepository : IAdminRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public AdminRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> CityExistsAsync(int cityId) =>
+        await _context.Cities.AnyAsync(c => c.Id == cityId);
+
+    public async Task<bool> HotelExistsAsync(int hotelId) =>
+        await _context.Hotels.AnyAsync(h => h.Id == hotelId);
+
+    public async Task AddHotelAsync(Hotel hotel) =>
+        await _context.Hotels.AddAsync(hotel);
+
+    public async Task AddRoomAsync(Room room) =>
+        await _context.Rooms.AddAsync(room);
+
+    public async Task AddRoomTypeAsync(RoomType type) =>
+        await _context.RoomTypes.AddAsync(type);
+
+    public async Task AddDiscountAsync(Discount discount) =>
+        await _context.Discounts.AddAsync(discount);
+
+    public async Task SaveChangesAsync() =>
+        await _context.SaveChangesAsync();
+}
